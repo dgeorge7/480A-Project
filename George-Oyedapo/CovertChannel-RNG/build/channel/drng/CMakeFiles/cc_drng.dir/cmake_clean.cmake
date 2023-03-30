250d10e852f40337ed707c3b5925a88c87ea7150
Joseph Oyedapo <oyedapojoseph@gmail.com>
2023-03-30 03:39:55 -0400

    latest

---
+++ George-Oyedapo/CovertChannel-RNG/build/channel/drng/CMakeFiles/cc_drng.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cc_drng.dir/drng.cpp.o"
+  "CMakeFiles/cc_drng.dir/drng.cpp.o.d"
+  "cc_drng"
+  "cc_drng.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cc_drng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
